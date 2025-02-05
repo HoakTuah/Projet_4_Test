dc732c52436574947e187fda87be2c726be712e3
HoakTuah <balcouille36@gmail.com>
2025-02-05 15:51:02 +0100

    Test réalisé sur le front end pour login & Register

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -7,19 +7,41 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 import { expect } from '@jest/globals';
+import { of, throwError } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
 import { SessionService } from 'src/app/services/session.service';
+import { Router } from '@angular/router';
 
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authServiceMock: any;   // ADD mock for AuthService
+  let routerMock: any;        // ADD mock for Router
 
   beforeEach(async () => {
+    
+    // Mock for login method
+    authServiceMock = {
+      login: jest.fn()    
+    };
+
+    // Mock for navigate method
+    routerMock = {
+      navigate: jest.fn() 
+    };
+
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      providers: [SessionService],
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },    // Mock Injector for AuthService
+        { provide: Router, useValue: routerMock },              // Mock Injector for Router
+        SessionService
+      ],
+      
       imports: [
         RouterTestingModule,
         BrowserAnimationsModule,
@@ -31,6 +53,7 @@ describe('LoginComponent', () => {
         ReactiveFormsModule]
     })
       .compileComponents();
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -39,4 +62,74 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  //=============================================================================================================
+  //==============================  Test for navigation after successful login
+  //=============================================================================================================
+
+   it('should navigate to sessions page on successful login', () => {
+
+    authServiceMock.login.mockReturnValue(of({ userId: '1', token: 'abc' }));     // Mock the login method of authService to simulate a successful login response
+    component.form.setValue({ email: 'toto3@toto.com', password: 'test!1234' });  // Set valid credentials in the form
+    component.submit();                                                           // Call the submit method
+
+    expect(component.form.valid).toBeTruthy();                                    // Check if the form is valid  
+    expect(authServiceMock.login).toHaveBeenCalled();                             // Check if the login method was called
+    expect(routerMock.navigate).toHaveBeenCalledWith(['/sessions']);              // Check if you can navigate to the '/sessions' page after successful login
+  });
+
+  //=============================================================================================================
+  //==============================  Test for error handling when login fails
+  //=============================================================================================================
+
+  it('should set onError to true on login failure', () => {
+    authServiceMock.login.mockReturnValue(throwError(() => new Error('Invalid credentials')));     // Mock the login method of authService to simulate a login failure response
+    component.form.setValue({ email: 'toto3@toto.com', password: 'test!1234' });                   // Set valid credentials in the form
+    component.submit();                                                                            // Trigger the submit function
+    fixture.detectChanges();                                                                       // Update view with validation feedback
+
+    expect(component.form.valid).toBeTruthy();                                                    // Check if the form is valid               
+    const errorMsg = fixture.debugElement.query(By.css('.error'));                                // Check if the error message is displayed
+    expect(component.onError).toBeTruthy();                                                       // Check if the onError property is set to true
+    expect(errorMsg.nativeElement.textContent).toContain('An error occurred');                    // Check if the error message contains the correct text
+  });
+
+  //=============================================================================================================
+  //==============================  Check if a field is left empty
+  //=============================================================================================================
+
+  it('should display red color on empty and touched email and password field', () => {
+
+    //===========================================================
+    //======================== Email field test
+    //===========================================================
+
+    const emailInput = fixture.debugElement.query(By.css('input[formControlName="email"]'));
+    emailInput.nativeElement.value = '';                            // Set invalid value
+    emailInput.nativeElement.dispatchEvent(new Event('input'));     // Simulate user entering value
+    emailInput.triggerEventHandler('blur', null);                   // Simulate user leaving the field (touching)
+    fixture.detectChanges();                                        // Update view with validation feedback
+
+    // Check if the error class is applied to Email
+    expect(emailInput.nativeElement.classList.contains('ng-invalid')).toBeTruthy();
+    expect(emailInput.nativeElement.classList.contains('ng-touched')).toBeTruthy();
+
+    //===========================================================
+    //======================== Password field test
+    //===========================================================
+
+    const passwordInput = fixture.debugElement.query(By.css('input[formControlName="password"]'));
+    passwordInput.nativeElement.value = '';                         // Set invalid value
+    passwordInput.nativeElement.dispatchEvent(new Event('input'));  // Simulate user entering value
+    passwordInput.triggerEventHandler('blur', null);                // Simulate user leaving the field (touching)
+    fixture.detectChanges();                                        // Update view with validation feedback
+
+    // Check if the error class is applied to password
+    expect(passwordInput.nativeElement.classList.contains('ng-invalid')).toBeTruthy();
+    expect(passwordInput.nativeElement.classList.contains('ng-touched')).toBeTruthy();
+
+  });
+
+
+
 });
--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -5,18 +5,38 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { of, throwError } from 'rxjs';
 
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let authServiceMock: any;
+  let routerMock: any;
 
   beforeEach(async () => {
+
+
+    authServiceMock = {
+      register: jest.fn()                                   // Mock for register method
+    };
+    routerMock = {
+      navigate: jest.fn()                                   // Mock for navigate method
+    };
+
     await TestBed.configureTestingModule({
       declarations: [RegisterComponent],
+      providers: [
+        { provide: AuthService, useValue: authServiceMock }, // Mock Injector for AuthService
+        { provide: Router, useValue: routerMock }            // Mock Injector for Router      
+      ],
       imports: [
         BrowserAnimationsModule,
         HttpClientModule,
@@ -24,7 +44,8 @@ describe('RegisterComponent', () => {
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
-        MatInputModule
+        MatInputModule,
+        RouterTestingModule
       ]
     })
       .compileComponents();
@@ -37,4 +58,124 @@ describe('RegisterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  //=============================================================================================================
+  //==============================  Function to test registration
+  //============================================================================================================= 
+
+  function testAccountCreation(formData: any, shouldSucceed: boolean) {
+
+    authServiceMock.register.mockReturnValue(shouldSucceed ? of({}) : throwError(() => new Error('Failed to register')));
+    component.form.setValue(formData);
+    component.submit();
+    fixture.detectChanges();
+
+    if (shouldSucceed) {
+      expect(authServiceMock.register).toHaveBeenCalledWith(formData);
+      expect(routerMock.navigate).toHaveBeenCalledWith(['/login']);
+    } else {
+      expect(authServiceMock.register).toHaveBeenCalledWith(formData);
+      expect(routerMock.navigate).not.toHaveBeenCalled();
+      expect(component.onError).toBeTruthy();  
+    }
+  }
+
+  it('should handle valid registration', () => {
+    testAccountCreation({
+      firstName: 'Toto',
+      lastName: 'Titi',
+      email: 'toto5@toto.com',
+      password: 'test!1234'
+    }, true);
+  });
+
+  it('should handle registration with missing fields', () => {
+    testAccountCreation({
+      firstName: '',
+      lastName: 'Titi',
+      email: 'toto5@toto.com',
+      password: 'test!1234'
+    }, false);
+  });
+
+  it('should handle registration with invalid email', () => {
+    testAccountCreation({
+      firstName: 'Toto',
+      lastName: 'Titi',
+      email: 'invalid-email',
+      password: 'test!1234'
+    }, false);
+  });
+
+  it('should handle registration short Password', () => {
+    testAccountCreation({
+      firstName: 'Toto',
+      lastName: 'Titi',
+      email: 'toto5@toto.com',
+      password: 'Pa'
+    }, false);
+  });
+
+  it('should handle registration too long Password', () => {
+    testAccountCreation({
+      firstName: 'Toto',
+      lastName: 'Titi',
+      email: 'toto5@toto.com',
+      password: 'Password123!Password123!Password123!Password123!Password123!Password123!Password123!'
+    }, false);
+  });
+  
+//=============================================================================================================
+//==============================  Function to test each field
+//=============================================================================================================
+
+function testInputField(fieldName: string, fieldValue: string, expectedValidity: boolean) {
+  const input = fixture.debugElement.query(By.css(`input[formControlName="${fieldName}"]`));
+  input.nativeElement.value = fieldValue;                       // Set value
+  input.nativeElement.dispatchEvent(new Event('input'));        // Simulate user entering value
+  input.triggerEventHandler('blur', null);                      // Simulate user leaving the field (touching)
+  fixture.detectChanges();                                      // Update view with validation feedback
+
+  // Check if the error class is applied
+  expect(input.nativeElement.classList.contains('ng-invalid')).toBe(!expectedValidity); 
+  expect(input.nativeElement.classList.contains('ng-touched')).toBeTruthy();
+  }
+
+//=============================================================================================================
+//==============================  Test First Name field
+//=============================================================================================================
+
+  it('should validate First Name field correctly', () => {
+    testInputField('firstName', '', false);     // Empty first name
+    testInputField('firstName', 'Toto', true);  // Valid first name
+  });
+
+//=============================================================================================================
+//==============================  Test Last Name field
+//=============================================================================================================
+
+it('should validate Last Name field correctly', () => {
+  testInputField('lastName', '', false);     // Empty last name
+  testInputField('lastName', 'Titi', true);  // Valid last name
+});
+
+//=============================================================================================================
+//==============================  Test Email field
+//=============================================================================================================
+
+it('should validate Email field correctly', () => {
+  testInputField('email', '', false);     // Empty email
+  testInputField('email', 'invalid-email', false);  // Incorrect email format
+  testInputField('email','toto5@toto.com', true);   // Valid email
+});
+
+//=============================================================================================================
+//==============================  Test Password field
+//=============================================================================================================
+
+it('should validate Password field correctly', () => {
+  testInputField('password', '', false);          // Empty password
+  testInputField('password','test!1234', true);   // Valid password
+});
+
 });
